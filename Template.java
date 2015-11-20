36a0b4e3ea303b2a29475d647685932edf5473ce
Kevin Orr <kevinorr54@gmail.com>
2015-11-20 11:19:18 -0500

    Updated Template.java

--- Template.java
+++ Template.java
@@ -1,9 +1,8 @@
-/*
-Student: Kevin Orr U45623430
-Class:   COP 2510-004
-Program: 
-Desc:    
-*/
+/* Student: Kevin Orr U45623430
+ * Class:   COP 2510-004
+ * Program: 
+ * Desc:    
+ */
 
 public class Template {
     public static void main(String[] args) {
