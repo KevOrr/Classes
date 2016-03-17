767cc27d00d51df62c5fa3757b6e5f5446fad93c
Kevin Orr <kevinorr54@gmail.com>
2016-03-17 19:19:14 -0400

    Added Bluetooth Functionality
    * Renamed label to just "Planetarium"
    * Better error reporting to user
    * Added string refs for error reporting
    * TODO implement device discovery instead of hardcoded MAC address

--- mobile/src/main/java/com/kcorr/planetariumremote/MainActivity.java
+++ mobile/src/main/java/com/kcorr/planetariumremote/MainActivity.java
@@ -1,7 +1,12 @@
 package com.kcorr.planetariumremote;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -17,15 +22,27 @@ import com.mhuss.AstroLib.ObsInfo;
 import com.mhuss.AstroLib.PlanetData;
 import com.mhuss.AstroLib.Planets;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final int[] PLANETS = {Planets.MERCURY, Planets.VENUS, Planets.EARTH};
+    private final String TAG = "MainActivity";
+    private final UUID BT_UUID = UUID.fromString("0000110E-0000-1000-8000-00805F9B34FB");
+    private final String BT_REMOTE_MAC = ""; // TODO device discovery
 
     private ImageButton rewindButton;
     private ImageButton forwardButton;
     private DatePicker datePicker;
     private FloatingActionButton sendFab;
 
-    private final int[] PLANETS = {Planets.MERCURY, Planets.VENUS, Planets.EARTH};
-    private final String TAG = "MainActivity";
+    private BluetoothAdapter btAdapter = (BluetoothAdapter) getSystemService(BLUETOOTH_SERVICE);
+    private BluetoothDevice btDevice = btAdapter.getRemoteDevice(BT_REMOTE_MAC);
+    private BluetoothSocket btSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +57,28 @@ public class MainActivity extends AppCompatActivity {
         this.sendFab = (FloatingActionButton) findViewById(R.id.send);
 
         addClickListeners();
+
+        // Connect to device
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    // TODO device discovery instead of hardcoded MAC address
+                    btSocket = btDevice.createRfcommSocketToServiceRecord(BT_UUID);
+                    btSocket.connect();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Snackbar.make(findViewById(R.id.main_activity_top), R.string.msg_connected,
+                                    Snackbar.LENGTH_LONG).show();
+                        }
+                    });
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException thrown while attempting to connect to device", e);
+                    Snackbar.make(findViewById(R.id.main_activity_top), R.string.msg_connect_failed,
+                            Snackbar.LENGTH_LONG);
+                }
+            }
+        }).start();
     }
 
     private void addClickListeners() {
@@ -69,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void calcSendPositions() {
+
+        // Get positions from AstroLib
         double positions[][] = getHelioPositions(datePicker.getDayOfMonth(),
                 datePicker.getMonth(), datePicker.getYear());
         if (BuildConfig.DEBUG) {
@@ -83,11 +124,35 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, output);
         }
 
+        // Open an OutputStream
+        OutputStream outStream;
+        try {
+            outStream = this.btSocket.getOutputStream();
+        } catch (IOException e) {
+            Log.e(TAG, "IOException thrown while attempting to open OutputStream to device", e);
+            Snackbar.make(findViewById(R.id.main_activity_top), R.string.msg_connect_failed,
+                    Snackbar.LENGTH_LONG);
+            return;
+        }
+
+        // Send a line for each planet
         for (double planet[] : positions) {
-            String payload = "" + (int) planet[0] + " " + planet[1] + " " + planet[2];
+            // Build payload
+            String payload = "" + (int) planet[0] + " " + planet[1] + " " + planet[2] + "\n";
             if (BuildConfig.DEBUG)
                 Log.d(TAG, "Sending this payload: \"" + payload + "\"");
-            // TODO send bluetooth
+
+            try {
+                if (Build.VERSION.SDK_INT >= 19)
+                    outStream.write(payload.getBytes(StandardCharsets.US_ASCII));
+                else
+                    outStream.write(payload.getBytes(Charset.forName("US-ASCII")));
+            } catch (IOException e) {
+                Log.e(TAG, "IOException thrown while attempting to send to OutputStream", e);
+                Snackbar.make(findViewById(R.id.main_activity_top), R.string.msg_send_failed,
+                        Snackbar.LENGTH_LONG);
+                return;
+            }
         }
     }
 
