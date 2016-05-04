ac661eee512a456ea045d211734d6f2ad4e41b14
Kevin Orr <kevinorr54@gmail.com>
2016-05-04 19:59:08 -0400

    Added a bunch of comments as per assignment info
    * Also added a better buffer overflow check

--- planetarium-arduino/planetarium-arduino.ino
+++ planetarium-arduino/planetarium-arduino.ino
@@ -2,16 +2,16 @@
 #include <AccelStepper.h>
 #include <TimerOne.h>
 
-#define DEBUG 1
+#define DEBUG 1 // set to 0 if you dont want the ATMega to send debug info
 
-#define GEAR_RATIO      2
-#define STEPS_PER_STEPPER_REV 4096
+#define GEAR_RATIO      2 // 2 planet column revolutions for 1 stepper revolution
+#define STEPS_PER_STEPPER_REV 4096 // 4096 half-steps per stepper revolution
 #define STEPS_PER_REV   STEPS_PER_STEPPER_REV * GEAR_RATIO
-#define MAXSPEED        600     // pulse/sec
-#define MAXACCEL        1000    // pulse/sec^2
+#define MAXSPEED        600     // max stepper speed in half-steps/sec
+#define MAXACCEL        1000    // max stepper acceleration in half-steps/sec^2
 #define N               3       // number of planets
 
-AccelStepper *steppers[3];
+AccelStepper *steppers[3]; // pointer array to each plannet
 AccelStepper mercury = AccelStepper(AccelStepper::HALF4WIRE, A2, A4, A3, A5); // 28BYJ-48
 AccelStepper venus = AccelStepper(AccelStepper::HALF4WIRE, 9, 11, 10, 12); // 28BYJ-48
 AccelStepper earth = AccelStepper(AccelStepper::HALF4WIRE, 5, 7, 6, 8); // 28BYJ-48
@@ -20,7 +20,7 @@ char buf[101] = {0}; // Temporary input buffer
 uint8_t i = 0; // input buffer char index
 
 void setup() {
-  Serial.begin(38400);
+  Serial.begin(38400); // this is what the HC-05 UART baud rate is set to
 
   // Store pointers to the steppers to easily pass to functions, iterate over, etc
   steppers[0] = &mercury;
@@ -34,24 +34,23 @@ void setup() {
   }
 
   // stepper.run must be called very often, but at least once per step cycle
-  Timer1.initialize(100);
+  Timer1.initialize(100); // once every 100 microseconds
   Timer1.attachInterrupt(runSteppers);
 
-  // Column headers
+  // Print column headers
   if (DEBUG) Serial.println(F("Stepper    Pos   Dist     Target"));
 }
 
 void loop() {
 
   while (Serial.available()) { // There's input, let's read it
-
     char c = Serial.read(); // Get one character
 
     if (c == '\r')
       continue; // discard carriage returns
-    else if ((c != '\n') && (i < sizeof(buf) - 1))
+    else if ((c != '\n') && (i < sizeof(buf) - 1)) // avoid buffer overflow
       buf[i++] = c; // store anything else
-    else {
+    else if (c == '\n') { // end of command
       buf[i] = '\0'; // Terminate with NUL
       i = 0; // Begin at start of buf next time
 
@@ -64,6 +63,9 @@ void loop() {
       }
 
       moveSteppers(targets);
+    } else { // buffer must have overflown
+      if (DEBUG) Serial.println("Buffer overflow!");
+      i = 0; // ignore the command
     }
   }
 }
@@ -75,29 +77,37 @@ void moveSteppers(int targets[]) {
 
   for (int i=0; i<N; i++) {
     int pos = steppers[i]->currentPosition();
-    pos %= STEPS_PER_REV;
+    pos %= STEPS_PER_REV; // reset position to mod(currPos, STEPS_PER_REV)
 
-    targets[i] %= STEPS_PER_REV;
+    // Calculate distance to each target
+    targets[i] %= STEPS_PER_REV; // make sure each target is in 0..STEPS_PER_REV
     int dist = targets[i] - pos;
     if (abs(STEPS_PER_REV - dist) < abs(dist))
-      dist = dist - STEPS_PER_REV;
+      dist = dist - STEPS_PER_REV; // if going the other way is closer, do it
     step_disp[i] = dist;
 
+    // Find maximum distance
     if (abs(dist) > maxDistance)
       maxDistance = abs(dist);
   }
 
   for (int i=0; i<N; i++) {
+    AccelStepper *stepper = steppers[i]; // get current stepper
+
     int pos = steppers[i]->currentPosition();
     pos %= STEPS_PER_REV;
-    float velocity = MAXSPEED * (float) abs(step_disp[i]) / (float) maxDistance;
 
-    AccelStepper *stepper = steppers[i];
+    // v_i = v_max * d_i / d_max
+    float velocity = MAXSPEED * (float) abs(step_disp[i]) / (float) maxDistance;
     stepper-> setMaxSpeed(velocity);
+
+    // a_i = a_max * v_i / v_max
     stepper-> setAcceleration(MAXACCEL * velocity / (float) MAXSPEED);
+
     stepper-> setCurrentPosition(pos);
     stepper-> move(step_disp[i]);
 
+    // Send debug info
     if (DEBUG) {
       char str[101];
       sprintf(str, "Stepper %1d: @%-4d %+-5d -> %-4d\r\n", i, pos, step_disp[i], targets[i]);
@@ -111,4 +121,3 @@ void runSteppers() {
   for (int i=0; i<N; i++)
     steppers[i] -> run();
 }
-
