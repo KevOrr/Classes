1403589d0012b820f749172b8c29f0860f32d1cb
Kevin Orr <kevinorr54@gmail.com>
2016-12-16 19:19:49 -0500

    Added process.py for logistic_regression class

---
+++ udemy/logistic_regression/process.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+def get_data(f):
+    df = pd.read_csv(f)
+    data = df.as_matrix()
+
+    X = data[:, :-1]
+    Y = data[:, -1]
+
+    # Normalize N-products_viewed and Visit_duration
+    X[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+    X[:,2] = (X[:,2] - X[:,2].mean()) / X[:,2].std()
+
+    # One-hot encode time
+    n,_ = X.shape
+    onehot_times = np.zeros((n, 4))
+    onehot_times[np.arange(n), X[:,4].astype(int)] = 1
+    X = np.concatenate((X[:, :-1], onehot_times), axis=1)
+
+    return X, Y
+
+def get_binary_data(X, Y):
+    return X[Y <= 1], Y[Y <= 1]
