df1a0787d3db0b86503b3a08323dc1ff8e3e3546
Kevin Orr <kevinorr54@gmail.com>
2017-02-23 04:50:51 -0500

    Working on memory_manager

--- data_structures/memory_manager/MemoryManager.cpp
+++ data_structures/memory_manager/MemoryManager.cpp
@@ -3,56 +3,85 @@
 #include "dlUtils.h"
 #include "MemoryManager.h"
 
-MemoryManager::MemoryManager(unsigned int memtotal): memsize(memtotal)
-{
-   baseptr = new unsigned char[memsize];
-   blockdata dummyBlock(0,false,0);
-   blockdata originalBlock(memsize,true,baseptr);
-   header = new dlNode<blockdata>(dummyBlock,nullptr,nullptr);
-   trailer = new dlNode<blockdata>(dummyBlock,nullptr,nullptr);
-   header->next = new dlNode<blockdata>(originalBlock,header,trailer);
-   trailer->prev = header->next;
+MemoryManager::MemoryManager(unsigned int memtotal)
+    : memsize(memtotal),
+      baseptr(new unsigned char[memsize]) {
+
+    blockdata dummyBlock(0, false, 0);
+    header = new dlNode<blockdata>(dummyBlock, nullptr, nullptr);
+    trailer = new dlNode<blockdata>(dummyBlock, nullptr, nullptr);
+
+    blockdata originalBlock(memsize, true, baseptr);
+    header->next = new dlNode<blockdata>(originalBlock, header, trailer);
+    trailer->prev = header->next;
 }
 
-MemoryManager::~MemoryManager()
-{
-  delete [] baseptr;
-  clearList(header);
+MemoryManager::~MemoryManager() {
+    delete [] baseptr;
+    clearList(header);
 }
 
-void MemoryManager::showBlockList() 
-{
-  printDlList(header,trailer,"->");
+void MemoryManager::showBlockList()  {
+    printDlList(header,trailer,"->");
 }
 
-void MemoryManager::splitBlock(dlNode<blockdata> *p, unsigned int chunksize)
-{
-  // Complete the code for this method
-  
+void MemoryManager::splitBlock(dlNode<blockdata> *p, unsigned int chunksize) {
+    assert(chunksize <= p->info.blocksize);
+    if (chunksize == 0)
+        return;
+    else if (chunksize == p->info.blocksize) {
+        p->info.free = false;
+        return;
+    }
+
+    dlNode<blockdata> *next = p->next;
+    blockdata unused(p->info.blocksize - chunksize, true, p->info.blockptr + chunksize);
+    p->next = new dlNode<blockdata>(unused, p, next);
+    next->prev = p->next;
 }
 
-unsigned char * MemoryManager::malloc(unsigned int request)
-{
-  // Complete the code for this method
+unsigned char * MemoryManager::malloc(unsigned int request) {
+    for (dlNode<blockdata> *cur = header->next; cur != trailer; cur = cur->next) {
+        if (cur->info.free && cur->info.blocksize >= request) {
+            splitBlock(cur, request);
+            return cur->info.blockptr;
+        }
+    }
 
+    return nullptr;
 }
 
-void MemoryManager::mergeForward(dlNode<blockdata> *p)
-{
-  // Complete the code for this method
+void MemoryManager::mergeForward(dlNode<blockdata> *p) {
+    dlNode<blockdata> *hold = p->next;
 
+    p->info.blocksize += hold->info.blocksize;
+    p->next = hold->next;
+    hold->next->prev = p;
+
+    delete hold;
 }
 
-void MemoryManager::mergeBackward(dlNode<blockdata> *p)
-{ 
-  // Complete the code for this method
+void MemoryManager::mergeBackward(dlNode<blockdata> *p) {
+    dlNode<blockdata> *hold = p->prev;
 
+    p->info.blocksize += hold->info.blocksize;
+    p->info.blockptr = hold->info.blockptr;
+    p->prev = hold->prev;
+    hold->prev->next = p;
+
+    delete hold;
 }
 
-void MemoryManager::free(unsigned char *ptr2block)
-{
-  // Complete the code for this method
+void MemoryManager::free(unsigned char *ptr2block) {
+    for (auto it=header->next; it != trailer; it = it->next) {
+        if (it->info.blockptr == ptr2block) {
+            if (it->prev->info.free)
+                mergeBackward(it);
+            if (it->next->info.free)
+                mergeForward(it);
 
+            it->info.free = true;
+            return;
+        }
+    }
 }
-
-
--- data_structures/memory_manager/testMemMgr.cpp
+++ data_structures/memory_manager/testMemMgr.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <random>
 #include "MemoryManager.h"
 
 int main() {
    MemoryManager heaper(100);
-   std::cout << "heap initialized\n";
+   std::cout << "Heap initialized" << std::endl;
    heaper.showBlockList();
 
-   
+   std::vector<unsigned char *> chunks;
+
+   std::default_random_engine gen;
+   std::uniform_int_distribution<unsigned int> distr(0, 20);
+   for (int i=0; i<10; ++i) {
+       unsigned int size = distr(gen);
+       std::cout << "Requesting memory size " << size << "..." << std::endl;
+       unsigned char *p = heaper.malloc(size);
+
+       if (p) {
+           chunks.push_back(p);
+           heaper.showBlockList();
+       } else {
+           std::cout << " FAILED" << std::endl;
+       }
+   }
+
+   while (auto n = chunks.size()) {
+       
+   }
 
    return 0;
 }
