42a063bbf44598ceb8dfd18f4c776e8ab1d8d09c
Kevin Orr <kevinorr54@gmail.com>
2017-02-24 17:20:33 -0500

    Final changes for data_structures/memory_manager

--- data_structures/memory_manager/testMemMgr.cpp
+++ data_structures/memory_manager/testMemMgr.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <ctime>
+
 #include "MemoryManager.h"
 
 int main() {
-   MemoryManager heaper(100);
+   MemoryManager heaper(50);
    std::cout << "Heap initialized" << std::endl;
    heaper.showBlockList();
 
    std::vector<unsigned char *> chunks;
 
-   std::default_random_engine gen;
+   std::default_random_engine gen(time(0));
    std::uniform_int_distribution<unsigned int> distr(0, 20);
    for (int i=0; i<10; ++i) {
        unsigned int size = distr(gen);
-       std::cout << "Requesting memory size " << size << "..." << std::endl;
+       std::cout << "Requesting memory size " << size << "...";
        unsigned char *p = heaper.malloc(size);
 
        if (p)
            chunks.push_back(p);
        else
-           std::cout << " FAILED" << std::endl;
+           std::cout << " FAILED";
 
+       std::cout << std::endl;
        heaper.showBlockList();
    }
 
    while (auto still_allocated = chunks.size()) {
        auto choice = std::uniform_int_distribution<std::vector<unsigned char *>::size_type>(0, still_allocated-1)(gen);
-       std::cout << "Freeing " << chunks[choice];
+       std::cout << "Freeing " << (void *) chunks[choice] << std::endl;
        heaper.free(chunks[choice]);
        chunks.erase(chunks.begin() + choice);
        heaper.showBlockList();
