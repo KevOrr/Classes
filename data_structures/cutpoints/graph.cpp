d0d392466f7191b1c2f4bed0b62282ee46fcdf3d
Kevin Orr <kevinorr54@gmail.com>
2017-04-30 20:02:21 -0400

    Completed data_structures/cutpoints

--- data_structures/cutpoints/graph.cpp
+++ data_structures/cutpoints/graph.cpp
@@ -2,68 +2,71 @@
 #include <cassert>
 #include <stack>
 
-graph::graph()
-{
-  int vertex;
-  cin >> size;
-  adjList.resize(size,NULL);
-  for(int i = 0; i < size; ++i) {
-    cin >> vertex;
-    while(vertex != -1) {
-      adjList[i] = new vnode(vertex,adjList[i]); // insert at begining
-      cin >> vertex;
+graph::graph() {
+    int vertex;
+    cin >> size;
+    adjList.resize(size,NULL);
+    for(int i = 0; i < size; ++i) {
+        cin >> vertex;
+        while(vertex != -1) {
+            adjList[i] = new vnode(vertex,adjList[i]); // insert at begining
+            cin >> vertex;
+        }
     }
-  }
 }
 
-int firstFalse(vector<bool> b)
-{
-  int i = 0;
-  while(i < b.size() && b[i])
-    i += 1;
-  return i;
-}
- 
-bool all(vector<bool> b)
-{
-  for(int i = 0; i < b.size(); ++i)
-    if(!b[i])
-      return false;
-  return true;
+int firstFalse(vector<bool> b) {
+    size_t i = 0;
+    while(i < b.size() && b[i])
+        i += 1;
+    return i;
 }
 
-void graph::dfs(vector<bool> &visited)
-{
-  int start = firstFalse(visited);
-  int nbr, current = start;
-  stack<int> S;
-  vnodeptr cursor;
-  visited[start] = true;
-  S.push(start);
-  // Supply the remaining code below
+bool all(vector<bool> b) {
+    for(size_t i = 0; i < b.size(); ++i)
+        if(!b[i])
+            return false;
+    return true;
+}
+
+void graph::dfs(vector<bool> &visited) {
+    int start = firstFalse(visited);
+    stack<int> S;
+    visited[start] = true;
+    S.push(start);
+
+    while (S.size()) {
+        int el = S.top();
+        S.pop();
+
+        visited[el] = true;
+
+        for (vnode * nextAdj = adjList[el]; nextAdj != nullptr; nextAdj = nextAdj->next) {
+            if (!visited[nextAdj->vertex]) {
+                S.push(nextAdj->vertex);
+                visited[nextAdj->vertex] = true;
+            }
+        }
+    }
+}
+
+
+bool graph::connected(int excluded = -1) {
+    vector<bool> visited(size,false);
+    if(excluded != -1)
+        visited[excluded] = true;
+
+    dfs(visited);
+    return all(visited);
 
 }
-  
 
-bool graph::connected(int excluded = -1)
-{
-  vector<bool> visited(size,false);
-  if(excluded != -1)
-    visited[excluded] = true;
+vector<int> graph::get_cutpoints() {
+    vector<int> cutpts;
 
-  // Supply the remaining code below
+    for (size_t i=0; i<adjList.size(); i++)
+        if (not connected(i))
+            cutpts.push_back(i);
 
-
-}  
-    
-vector<int> graph::get_cutpoints()
-{
-  vector<int> cutpts;
-  // Supply the remaining code below
-
-  
+    return cutpts;
 }
-  
-
-   
-  
--- data_structures/cutpoints/graph.h
+++ data_structures/cutpoints/graph.h
@@ -7,23 +7,23 @@
 using namespace std;
 
 struct vnode {
-  int vertex; 
-  vnode *next;
-  vnode(int u, vnode *n): vertex(u), next(n){};
+    int vertex;
+    vnode *next;
+    vnode(int u, vnode *n): vertex(u), next(n){};
 };
 
 typedef vnode * vnodeptr;
 
 class graph {
- public:
-  graph(); // interactive constructor using cin
-  bool connected(int excluded);
-  void dfs(vector<bool> &visited);
-  vector<int> get_cutpoints();
-    
- private:
-  int size;
-  vector<vnodeptr> adjList;
+public:
+    graph(); // interactive constructor using cin
+    bool connected(int excluded);
+    void dfs(vector<bool> &visited);
+    vector<int> get_cutpoints();
+
+private:
+    int size;
+    vector<vnodeptr> adjList;
 };
 
 #endif
