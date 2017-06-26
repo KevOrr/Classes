916bbdb1140b6560a2154149c2cfc49fdd388e1b
Kevin Orr <kevinorr54@gmail.com>
2017-06-25 21:20:02 -0400

    Started to write quicksort

--- algorithms/bonus-project/supplied/driver.cpp
+++ algorithms/bonus-project/supplied/driver.cpp
@@ -6,10 +6,10 @@
 #include <iostream>
 using namespace std;
 
-#define CLASS_TO_TEST Delusional
-const int POP_SIZE = 1000;
+#define CLASS_TO_TEST SlightlyOff
+const int POP_SIZE = 10000;
 
-const int NTRIALS = 1;
+const int NTRIALS = 5;
 const unsigned int RANDOM_SEED[] = {0xDEADBEEF, 0xC0C0ABEE, 0xFEEDFACE, 0xDEBEDDED, 0xABEADED1};
 
 int main()
--- algorithms/bonus-project/supplied/sort.cpp
+++ algorithms/bonus-project/supplied/sort.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdint>
 #include <utility>
 #include <algorithm>
 #include <iostream>
@@ -13,6 +14,64 @@ void print_array(Subject* arr, size_t n) {
     }
     std::cout << ")" << std::endl << std::endl;
 }
+
+template<typename T>
+uint_fast8_t median3(T a, T b, T c) {
+    if (a < b) {
+        if (b < c)
+            return b;
+        else if (a < c)
+            return c;
+        else
+            return a;
+    } else {
+        if (c < b)
+            return b;
+        else if (a < c)
+            return a;
+        else
+            return c;
+    }
+}
+
+template<typename T>
+size_t partition(T* arr, size_t low, size_t high) {
+    T p = (median3(arr[low], arr[(low + high)/2], arr[high]));
+
+    size_t left = low, right = high;
+    while (true) {
+        do {left++;} while (arr[left] < p);
+
+        do {right--;} while (arr[right] > p);
+
+        if (left >= right)
+            return right;
+        std::swap(arr[left], arr[right]);
+    }
+}
+
+template<typename T>
+void quicksort(T* arr, size_t low, size_t high) {
+    if (low < high) {
+        size_t p = partition(arr, low, high);
+        quicksort(arr, low, p);
+        quicksort(arr, p+1, high);
+    }
+}
+
+// int main() {
+//     int A[] = {1,6,7,5,3,3,4,6,8,9,7,6,3,4,67,357,34,256,46,24,36,23,246,34,34,1,143,1345,134,51345,3145,342,
+//                223,35,326,47,25,62,234,243,5,2,34,45,356,56,586,679,985,87,87,7,6,65,263,364,5,5,12,15,5,5326,
+//                6635,6,43,34,76,87,56,74,874,895,7089,978,86,7,6,341,45,32,32,65,65,76,748,78,7,7,643,652,532};
+
+//     size_t n = sizeof A / sizeof A[0];
+//     quicksort(A, 0, n-1);
+//     for (size_t i=0; i<n; i++) {
+//         std::cout << A[i] << " ";
+//     }
+
+//     std::cout << std::endl;
+// }
 
 void sort_relative(Subject* arr, size_t n) {
     std::pair<size_t, Subject*> counts[n];
@@ -43,8 +102,6 @@ void sort_extremes(Subject* arr, size_t n) {
     std::pair<size_t, Subject*> high_counts[n - high];
     std::pair<size_t, Subject*> low_counts[1 + low];
 
-    std::cout << high << " " << low << std::endl;
-
     for (size_t i=0; i<=low; i++) {
         size_t count = 0;
         for (size_t j = 1 + low; j < high; j++) {
@@ -71,16 +128,6 @@ void sort_extremes(Subject* arr, size_t n) {
                   return a.first < b.first;
               });
 
-    for (size_t i=0; i<5; i++) {
-        std::cout << high_counts[i].first << " " << high_counts[i].second->getCompetence() << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    for (size_t i=n - high - 6; i<n - high; i++) {
-        std::cout << high_counts[i].first << " " << high_counts[i].second->getCompetence() << std::endl;
-    }
-
     Subject low_temp[low + 1];
     for (size_t i = 0; i <= low; i++) {
         low_temp[i] = *low_counts[i].second;
@@ -92,13 +139,11 @@ void sort_extremes(Subject* arr, size_t n) {
         high_temp[i] = *high_counts[i].second;
     }
     std::copy(high_temp, high_temp + n - high, arr + high);
-    // print_array(arr, n);
 }
 
 void dksort(Subject* arr, int n) {
-    // print_array(arr, n);
+    for (int i = 0; i < n; i++)
+        arr[i].setCompetence(100.0 * i / n);
     sort_relative(arr, n);
-    // print_array(arr, n);
     sort_extremes(arr, n);
-    // print_array(arr, n);
 }
